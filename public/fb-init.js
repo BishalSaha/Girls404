8e9b53b57b6bb151e5e0a72c73847fb35867f41d
BISHAL SAHA <bishalboss557@gmail.com>
2020-10-08 12:51:56 +0530

    first commit

---
+++ public/fb-init.js
@@ -0,0 +1,18 @@
+// import 'firebase/app';
+// import * as fbAnalytics from 'firebase/analytics';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAzqDC93bK6yG9ACacmtOo1F5wZR4xH9kw",
+  authDomain: "girls404.firebaseapp.com",
+  databaseURL: "https://girls404.firebaseio.com",
+  projectId: "girls404",
+  storageBucket: "girls404.appspot.com",
+  messagingSenderId: "418844066054",
+  appId: "1:418844066054:web:4544dc11dab5c95d9c1e87",
+  measurementId: "G-B6Q6J0WQBZ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// fbAnalytics.analytics();
+firebase.analytics();
